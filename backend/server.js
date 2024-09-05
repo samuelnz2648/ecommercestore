9b6b7b7799fe7354d9c00d6996065b2dc97ec277
samuelnz2468 <samuel.christian.ong@gmail.com>
2024-09-06 10:57:46 +1200

    06/09/24 10:57am

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,37 @@
 // ecommercestore/backend/server.js
 
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+require("dotenv").config();
+const { errorHandler } = require("./middleware/errorMiddleware");
 
 const app = express();
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
-app.use(morgan('dev'));
+app.use(morgan("dev"));
 
 // Database
-const sequelize = require('./config/db');
-const models = require('./models');
+const sequelize = require("./config/db");
+const models = require("./models");
 
 // Sync all models
-sequelize.sync({ force: false })
-  .then(() => console.log('Database & tables created!'))
-  .catch(err => console.log('Error: ' + err));
+sequelize
+  .sync({ force: false })
+  .then(() => console.log("Database & tables created!"))
+  .catch((err) => console.log("Error: " + err));
 
 // Routes
-app.use('/api/users', require('./routes/userRoutes'));
-app.use('/api/products', require('./routes/productRoutes'));
-app.use('/api/cart', require('./routes/cartRoutes'));
-app.use('/api/orders', require('./routes/orderRoutes'));
+app.use("/api/users", require("./routes/userRoutes"));
+app.use("/api/products", require("./routes/productRoutes"));
+app.use("/api/cart", require("./routes/cartRoutes"));
+app.use("/api/orders", require("./routes/orderRoutes"));
+
+// Error Handling Middleware
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
 
