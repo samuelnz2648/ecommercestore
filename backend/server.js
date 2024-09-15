b1e48c7441c0504f8bb6abc8f7e6d64eca39e149
samuelnz2468 <samuel.christian.ong@gmail.com>
2024-09-16 10:22:56 +1200

    16/09/24 10:22am

--- backend/server.js
+++ backend/server.js
@@ -34,6 +34,6 @@ app.use("/api/upload", require("./routes/uploadRoutes"));
 // Error Handling Middleware
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 app.listen(PORT, console.log(`Server running on port ${PORT}`));
